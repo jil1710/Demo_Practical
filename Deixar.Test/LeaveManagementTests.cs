98f7393ac548c7ebf0b2fe8f90813553155d188e
unknown <jil.p@simformsolutions.com>
2023-09-06 13:41:37 +0530

    add: testing module func

---
+++ Deixar.Test/LeaveManagementTests.cs
@@ -0,0 +1,44 @@
+﻿using Deixar.API.Controllers.V2;
+using Deixar.Data.Contexts;
+using Deixar.Domain.DTOs;
+using Deixar.Domain.Entities;
+using Deixar.Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Security.Principal;
+
+namespace Deixar.Test
+{
+    public class LeaveManagementTests
+    {
+        public readonly Mock<ILeaveRepository> _leaveRepository = new();
+        public readonly Mock<IEmailUtility> _emailUtility = new();
+        public readonly Mock<ApplicationDBContext> _applicationContext = new();
+
+        [Fact]
+        public void AddLeaveRequest_Success()
+        {
+            // Arrange
+           
+            var leaveController = new LeaveManagementController(_leaveRepository.Object,_applicationContext.Object,_emailUtility.Object);
+
+            // Act
+            var leave = new LeaveRequestModel()
+            {
+                Reason = "Sick leave",
+                IsPaidLeave = true,
+                LeaveDate = DateTime.Now.AddDays(1)
+            };
+            var result = leaveController.AddLeaveRequestAsync(leave);
+            var objectResult = result.Result as ObjectResult;
+
+            // Assert
+            Assert.NotNull(objectResult);
+            Assert.True(objectResult is OkObjectResult);
+            Assert.Equal(StatusCodes.Status200OK, objectResult.StatusCode);
+
+            _leaveRepository.Verify(x => x.CreateLeaveRequestAsync(It.IsAny<Leave>()), Times.Once);
+        }
+    }
+}
